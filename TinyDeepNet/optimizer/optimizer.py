658a59df3c1aa73406317677afa39f198af080d1
flzq <freedomcoder@163.com>
2021-10-08 19:19:40 +0800

    增加AdaGrad

--- TinyDeepNet/optimizer/optimizer.py
+++ TinyDeepNet/optimizer/optimizer.py
@@ -7,12 +7,14 @@ from ..common.graph import Graph, get_node_from_graph
 
 class Optimizer(object):
     """
-    优化器基类
+    优化器基类：基于不同的梯度更新策略更新变量节点的 value
     """
 
     def __init__(self, graph, target, learning_rate=0.01):
         """
-        优化器的构造函数接受计算图对象，目标节点对象以及学习率
+        graph: 计算图对象，
+        target: 目标节点对象
+        learning_rate: 学习率
         """
         assert isinstance(target, Node) and isinstance(graph, Graph)
         self.graph = graph
@@ -81,7 +83,8 @@ class Optimizer(object):
 
     def forward_backward(self):
         """
-        完成一次前向传播计算结果节点的值并反向传播计算结果节点对各个节点的雅可比矩阵
+        首先完成一次前向传播，计算结果节点的值；
+        之后进行反向传播，计算结果节点对各个节点的雅可比矩阵；
         """
 
         # 清除计算图中所有节点的雅可比矩阵
@@ -106,7 +109,7 @@ class Optimizer(object):
 
 class GradientDescent(Optimizer):
     """
-    梯度下降优化器：朴素梯度下降法
+    梯度下降优化器：朴素梯度下降法--批量梯度下降法
     """
 
     def __init__(self, graph, target, learning_rate=0.01):
@@ -117,11 +120,12 @@ class GradientDescent(Optimizer):
     def _update(self):
         """
         朴素梯度下降法
+        优化器重写_update方法
         """
         for node in self.graph.nodes:
             if isinstance(node, Variable) and node.trainable:
 
-                # 取得该节点在当前批的平均梯度
+                # 取得该节点在当前小批量数据集中的平均梯度
                 gradient = self.get_gradient(node)
 
                 # 用朴素梯度下降法更新变量节点的值
@@ -131,7 +135,7 @@ class GradientDescent(Optimizer):
 
 class Momentum(Optimizer):
     """
-    冲量法
+    冲量法：通过累积历史梯度，调整当前的梯度方向
     """
 
     def __init__(self, graph, target, learning_rate=0.01, momentum=0.9):
@@ -151,15 +155,41 @@ class Momentum(Optimizer):
         for node in self.graph.nodes:
             if isinstance(node, Variable) and node.trainable:
 
-                # 取得该节点在当前批的平均梯度
+                # 取得该节点在当前batch_size的平均梯度
                 gradient = self.get_gradient(node)
 
                 if node not in self.v:
                     self.v[node] = gradient
                 else:
-                    # 滑动平均累积历史速度
+                    # 通过历史梯度调整当前梯度
                     self.v[node] = self.momentum * self.v[node] \
                         - self.learning_rate * gradient
 
                 # 更新变量节点的值
                 node.set_value(node.value + self.v[node])
+
+
+class AdaGrad(Optimizer):
+    """
+    AdaGrad优化器对梯度的每个分量采用了不同的学习率
+    """
+    def __init__(self, graph, target, learning_rate=0.01):
+        Optimizer.__init__(self, graph, target)
+        self.learning_rate = learning_rate
+        # 累加历史梯度各分量的平方的字典
+        self.s = dict()
+    
+    def _update(self):
+        for node in self.graph.nodes:
+            if isinstance(node, Variable) and node.trainable:
+                # 获得当前节点的平均梯度
+                gradient = self.get_gradient(node)
+
+                # 累加历史梯度的平方和
+                if node not in self.s:
+                    self.s[node] = np.power(gradient, 2)
+                else:
+                    self.s[node] = self.s[node] + np.power(gradient, 2)
+                
+                # 更新当前节点参数
+                node.set_value(node.value - self.learning_rate * gradient / (np.sqrt(self.s[node]+1e-10)))
